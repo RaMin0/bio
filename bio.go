83b49c98e342ed419c2d992fc6b9c5a59b45889a
Ramy Aboul Naga <ramy.naga@gmail.com>
2017-10-13 13:50:04 +0200

    Updated bio

--- bio.go
+++ bio.go
@@ -8,9 +8,10 @@ import (
 )
 
 var (
-	MajorRegexp = regexp.MustCompile(`(?i)^CSEN|DMET$`)
+	majorRegexp = regexp.MustCompile(`(?i)^CSEN|DMET$`)
 )
 
+// Bio holds the data for a bio
 type Bio struct {
 	id          string
 	name        string
@@ -18,9 +19,10 @@ type Bio struct {
 	major       string
 }
 
+// SetID sets the ID and returns an error if it is not valid
 func (bio *Bio) SetID(id string) error {
 	if !strings.Contains(id, "-") {
-		return fmt.Errorf("Failed in parsing ID \"%s\" as \"13-8994\". ", id)
+		return fmt.Errorf("ID should look like 13-8994: %v", id)
 	}
 
 	bio.id = id
@@ -28,33 +30,45 @@ func (bio *Bio) SetID(id string) error {
 	return nil
 }
 
-func (bio *Bio) ID() string {
+// ID returns the ID
+func (bio Bio) ID() string {
 	return bio.id
 }
 
-func (bio *Bio) SetName(name string) {
+// SetName sets the name and returns an error if it is empty
+func (bio *Bio) SetName(name string) error {
+	if name == "" {
+		return fmt.Errorf("name is empty")
+	}
+
 	bio.name = name
+
+	return nil
 }
 
-func (bio *Bio) Name() string {
+// Name returns the name
+func (bio Bio) Name() string {
 	return bio.name
 }
 
+// SetDateOfBirth sets the date of birth and returns an error if it is not valid
 func (bio *Bio) SetDateOfBirth(dateOfBirth string) (err error) {
 	if bio.dateOfBirth, err = time.Parse("02/01/2006", dateOfBirth); err != nil {
-		return fmt.Errorf("Failed in %s. ", strings.Split(err.Error(), ":")[0])
+		err = fmt.Errorf("%s: %v", strings.SplitN(err.Error(), ": ", 2)[1], dateOfBirth)
 	}
 
 	return
 }
 
-func (bio *Bio) DateOfBirth() time.Time {
+// DateOfBirth returns the date of birth
+func (bio Bio) DateOfBirth() time.Time {
 	return bio.dateOfBirth
 }
 
+// SetMajor sets the major and returns an error if it is not valid
 func (bio *Bio) SetMajor(major string) error {
-	if !MajorRegexp.MatchString(major) {
-		return fmt.Errorf("Failed in parsing major \"%s\" as \"CSEN\" or \"DMET\". ", major)
+	if !majorRegexp.MatchString(major) {
+		return fmt.Errorf("major should be either CSEN or DMET: %v", major)
 	}
 
 	bio.major = strings.ToUpper(major)
@@ -62,18 +76,22 @@ func (bio *Bio) SetMajor(major string) error {
 	return nil
 }
 
-func (bio *Bio) Major() string {
+// Major returns the major
+func (bio Bio) Major() string {
 	return bio.major
 }
 
-func (bio *Bio) Age() int {
+// Age returns the age
+func (bio Bio) Age() int {
 	return time.Now().Year() - bio.dateOfBirth.Year()
 }
 
-func (bio *Bio) FirstName() string {
+// FirstName returns the first name
+func (bio Bio) FirstName() string {
 	return strings.Fields(bio.name)[0]
 }
 
-func (bio *Bio) FormattedDateOfBith() string {
+// FormattedDateOfBith returns the formatted date of birth
+func (bio Bio) FormattedDateOfBith() string {
 	return bio.dateOfBirth.Format("Monday, January 02, 2006")
 }
